708b2deaf222a394aaeca49596121a02455fa691
Arismendy <2025-0296@unad.edu.do>
2025-11-13 20:00:10 -0400

    ejercicios de lista

--- vocal.py
+++ vocal.py
@@ -1,7 +1,15 @@
-# Pedir palabra y contar vocales
 palabra = input("Introduce una palabra: ")
 
-# Mostrar resultados
-print(f"\nEn '{palabra}' hay:")
-for vocal in 'aeiou':
-    print(f"- {vocal}: {palabra.count(vocal)}")
+
+vocales = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+
+for letra in palabra:
+    if letra in vocales:
+        vocales[letra] += 1
+
+
+print(f"\nEn la palabra '{palabra}' hay:")
+
+for vocal, cantidad in vocales.items():
+    
+    print(f"- {vocal}: {cantidad}")
