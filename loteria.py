64ef446851feae7588f17f242fb5647372f91581
Arismendy <2025-0296@unad.edu.do>
2025-11-13 20:18:17 -0400

    ejercicios de lista

--- loteria.py
+++ loteria.py
@@ -1,6 +1,11 @@
 Entrada = input("¿cuales son tus numeros de la loteria primitiva?(separados por espacios): ")
 
+
 numeros = [int(num) for num in Entrada.split()]
+
+if len(numeros) != 3:
+    print("Error: Debes ingresar exactamente 3 numeros.")
+    exit()
 
 numeros.sort()
 
